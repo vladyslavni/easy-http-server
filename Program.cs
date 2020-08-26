ef425a25c69fb270f1fdae500d6cb37bf099fc6e
vladyslavni <vladyslavnik@tahometer.com>
2020-08-26 14:14:29 +0300

    13 Environment: add: Take urls from environment variables

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-
+using System;
 namespace easy_http_server
 {
     public class Program
@@ -8,6 +8,9 @@ namespace easy_http_server
         public static void Main(string[] args)
         {
             Params.ApplyParameters(args);
+            string[] urls = GetEnvironmentUrls("HTTP_SERVER_URLS");
+            ConfigParams.UrlStorage.AddAll(urls);
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -16,9 +19,13 @@ namespace easy_http_server
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://0.0.0.0:" + ConfigParams.Port);
                     webBuilder.UseStartup<Startup>();
                 });
+        }
+
+        public static string[] GetEnvironmentUrls(string variableName)
+        {
+            return Environment.GetEnvironmentVariable(variableName).Split(",");
         }
     }
 }
